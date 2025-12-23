5ca4bc5d55d354e1535c171a2d8a132a8f935ab2
ayuxsec <249073442+ayuxsec@users.noreply.github.com>
2025-12-23 22:04:27 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,10 +2,13 @@
 # This script downloads my config files to their correct file locations
 
 base_url="https://raw.githubusercontent.com/ayuxsec/my_config_files/refs/heads/main"
-mkdir -p "$HOME/.config/my_config_files"
 
-curl -s -o "~/.config/conky/conky.conf" "${base_url}/conky.conf"
-curl -s -o "~/.config/my_config_files/ollama.bashrc" "${base_url}/ollama.bashrc" && \
-  printf "\n# source My config files\nsource ~/.config/my_config_files/ollama.bashrc\n" >> "$HOME/.bashrc"
+mkdir -p "$HOME/.config/my_config_files"
+mkdir -p "$HOME/.config/conky"
+
+curl -o "$HOME/.config/conky/conky.conf" -fLsS "$base_url/conky.conf"
+
+curl -o "$HOME/.config/my_config_files/ollama.bashrc" -fLsS "$base_url/ollama.bashrc" && \
+ printf '\n# source my config files\nsource "$HOME/.config/my_config_files/ollama.bashrc"\n' >> "$HOME/.bashrc"
 
 echo "[+] restart your shell."
