204565387d9c01ec1178c789c9f154bfccae7c47
ayuxsec <249073442+ayuxsec@users.noreply.github.com>
2025-12-23 23:34:30 +0530

    Add pentesting config file to installer script

--- installer.sh
+++ installer.sh
@@ -11,4 +11,7 @@ curl -o "$HOME/.config/conky/conky.conf" -fLsS "$base_url/conky.conf"
 curl -o "$HOME/.config/my_config_files/ollama.bashrc" -fLsS "$base_url/ollama.sh" && \
  printf '\n# source my config files\nsource "$HOME/.config/my_config_files/ollama.bashrc"\n' >> "$HOME/.bashrc"
 
+curl -o "$HOME/.config/my_config_files/pentesting.bashrc" -fLsS "$base_url/pentesting.sh" && \
+ printf '\nsource "$HOME/.config/my_config_files/pentesting.bashrc"\n' >> "$HOME/.bashrc"
+
 echo "[+] restart your shell."
