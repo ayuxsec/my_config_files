ec00983b685c10898c7ebf0eda7a6219a2ec4abd
ayuxsec <249073442+ayuxsec@users.noreply.github.com>
2025-12-23 21:48:30 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # This script downloads my config files to their correct file locations
 
 base_url="https://raw.githubusercontent.com/ayuxsec/my_config_files/refs/heads/main"
